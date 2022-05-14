7f0f9e4a3cb8f749a47835e88d1f69d6bb213a41
melaniepardo <melaniepardoes@gmail.com>
2022-05-14 12:13:25 -0400

    actualización

--- assets/index.js
+++ assets/index.js
@@ -22,9 +22,14 @@ const fs = require('fs')
 const getData = require('./getData')
 const { v4: uuidv4 } = require('uuid')
 
+const datosMail = (req) => {
+    const { correos, asunto, contenido } = url.parse(req.url, true).query
+    return { correos, asunto, contenido }
+}
+
 http
     .createServer(async function (req, res) {
-        let { correos, asunto, contenido } = url.parse(req.url, true).query
+        // let { correos, asunto, contenido } = url.parse(req.url, true).query
         if (req.url == '/') {
             res.setHeader('content-type', 'text/html')
             fs.readFile('index.html', 'utf8', (err, data) => {
@@ -33,6 +38,7 @@ http
         }
         if (req.url.startsWith('/mailing')) {
             const data = await getData()
+            const { correos, asunto, contenido } = await datosMail(req)
             const dolar = data.dolar.valor
             const euro = data.euro.valor
             const uf = data.uf.valor
@@ -40,7 +46,7 @@ http
             const mensaje = `El valor del dolar el día de hoy es: ${dolar} <br> El valor del euro el día de hoy es: ${euro} <br> El valor del uf el día de hoy es: ${uf} <br> El valor del utm el día de hoy es: ${utm}`
             if (correos !== '' && asunto !== '' && contenido !== '') {
                 enviar(correos.split(','), asunto, contenido + mensaje)
-                fs.writeFile(`${uuidv4()}.txt` , `${contenido + mensaje}`, (err) => {
+                fs.writeFile(`./correos/${uuidv4()}.txt`, `${contenido + mensaje}`, (err) => { //./correos/ sirve para guardar los archivos creados en carpeta correos
                     if (err) console.log(err)
                     else console.log('Archivo creado')
                 })
